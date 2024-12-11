b92d9cf6bb53ae880bca776d9a4ea0efa4576175
ckuehne <conny.kuehne-extern@deutschebahn.com>
2024-12-11 20:45:20 +0100

    use arrow keys to switch between buffers

--- core/prelude-editor.el
+++ core/prelude-editor.el
@@ -136,9 +136,9 @@
 
 (recentf-mode +1)
 
-;; use shift + arrow keys to switch between visible buffers
+;; use ctrl + arrow keys to switch between visible buffers
 (require 'windmove)
-(windmove-default-keybindings)
+(windmove-default-keybindings 'ctrl)
 
 ;; automatically save buffers associated with files on buffer switch
 ;; and on windows switch
